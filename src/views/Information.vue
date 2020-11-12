06cd28e0ba20d95d9e0997d0402bf4037e069412
Jolly <jolly96k@gmail.com>
2020-11-12 23:02:42 +0900

    Update views

---
+++ src/views/Information.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Information</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Information"
+};
+</script>
